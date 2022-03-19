fdc505dc6cb8759e3fcf9e431341af906f26604e
mehmetveyselsekendiz <mehmetveyselsekendiz@gmail.com>
2022-03-19 18:11:09 +0300

    ADD: book store is created

--- app.go
+++ app.go
@@ -23,19 +23,38 @@ type Book struct {
 }
 
 func main() {
-	b := Book{
-		id: 1,
-        name: "John Doe",
-		page: 5,
-		stock: 3,
-		price: 20.4,
-		stock_code: "sdsad",
-		ISBN: "123",
+
+	var book_store map[int]Book
+	book_store = make(map[int]Book)
+
+	b1 := Book{
+		id: len(book_store) + 1,
+        name: "Book1",
+		page: 111,
+		stock: 11,
+		price: 11.1,
+		stock_code: "BK-1",
+		ISBN: "11-11-11",
         Author: Author{
             id: 1,
-			name: "Author",
+			name: "Author1",
         },
     }
 
-	fmt.Println(b)
+	b2 := Book{
+		id: len(book_store) + 1,
+        name: "Book2",
+		page: 222,
+		stock: 22,
+		price: 22.2,
+		stock_code: "BK-2",
+		ISBN: "22-22-22",
+        Author: Author{
+            id: 2,
+			name: "Author2",
+        },
+    }
+
+	fmt.Println(b1)
+	fmt.Println(b2)
 }
